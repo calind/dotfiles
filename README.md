1b447362feae87aa3d11a41d95a9c360f34dbaaa
Calin Don <calin@presslabs.com>
2013-05-16 15:02:48 +0300

    Inital commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Installation
+
+**ATTENTION**: Before proceeding backup your `~/.bashrc`
+
+    git clone git@github.com:calind/dotfiles.git ~/.dotfiles
+    cd ~/.dotfiles
+    ./install.sh
---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+
+DOTFILES_DIR="$(dirname "$(readlink -f "$0")")"
+
+echo "Installing dotfiles into '$HOME'..."
+
+ln -sf "$DOTFILES_DIR/bash/bashrc" "$HOME/.bashrc"
+
+if [ ! -e "$DOTFILES_DIR/bash/exports.local" ] ; then
+    touch "$DOTFILES_DIR/bash/exports.local"
+    chmod +x "$DOTFILES_DIR/bash/exports.local"
+fi
+
+local_settings="$HOME/.settings"
+if [ ! -e "$local_settings" ] ; then
+    touch "$local_settings"
+    chmod +x "$local_settings"
+fi
+
+cat <<EOF > "$local_settings"
+export DOTFILES_DIR=$DOTFILES_DIR
+EOF
