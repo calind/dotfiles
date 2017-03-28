61717d10b45aae67c9693cf2eefbb8afbd0750e3
Calin Don <calin.don@gmail.com>
2017-03-28 15:31:15 +0300

    Added script for installing dotfiles

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 # Installation
 
-**ATTENTION**: Before proceeding backup your `~/.bashrc`, `~/.vimrc`, `~/.vim`
-and any other dotfiles you might want to save.
+```bash
+curl -Lks https://raw.githubusercontent.com/calind/dotfiles/master/bin/install-dotfiles.sh | /bin/bash
+```
 
-    git clone git@github.com:calind/dotfiles.git ~/.dotfiles
-    cd ~/.dotfiles
-    ./install.sh
+# Console font
 
-For the console I use
 [inconsolata-dz](http://nodnod.net/2009/feb/12/adding-straight-single-and-double-quotes-inconsola/)
---
+++ bin/install-dotfiles.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+function config {
+   /usr/bin/git --git-dir="$HOME/.dotfiles.git/" --work-tree="$HOME" "$@"
+}
+
+git clone --bare git@github.com:calind/dotfiles.git "$HOME/.dotfiles.git" || exit 1
+
+if [ ! -d "$HOME/.config-backup" ] ; then
+    mkdir -p "$HOME/.config-backup"
+fi
+
+if config checkout 2> /dev/null ; then
+    echo "Checked out config."
+else
+    echo "Backing up pre-existing dot files."
+    for path in $(config checkout 2>&1 | egrep '^\s+' | awk '{print $1}') ; do
+        echo "Backing up $path"
+        if [ ! -d "$(dirname "$HOME/.config-backup/$path")" ] ; then
+            mkdir -p "$(dirname "$HOME/.config-backup/$path")"
+        fi
+        mv "$path" "$HOME/.config-backup/$path"
+   done
+fi
+
+config checkout ; config submodule update --init --recursive
+config config --local status.showUntrackedFiles no
