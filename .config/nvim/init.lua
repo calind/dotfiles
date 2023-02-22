7521a59cafd4a2cb0f4cf5d51c7dd488be77c859
Calin Don <calin.don@gmail.com>
2023-02-22 18:40:42 +0200

    Fix formatting issues for nvim/init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -54,8 +54,8 @@ au('BufEnter', {
     end
 })
 
-au({'BufNewFile', 'BufRead'}, {
-    callback = function ()
+au({ 'BufNewFile', 'BufRead' }, {
+    callback = function()
         local path = vim.fs.normalize(fn.expand('%:p'))
         vim.pretty_print(path)
         if path:find('github.com/bitpoke') then
@@ -1106,7 +1106,7 @@ wk.register({
 --{{{ nvim-autopairs
 local npairs = require('nvim-autopairs')
 npairs.Rule = require('nvim-autopairs.rule')
-npairs.cond = require'nvim-autopairs.conds'
+npairs.cond = require 'nvim-autopairs.conds'
 npairs.setup({
     enable_check_bracket_line = true,
 })
@@ -1114,34 +1114,34 @@ npairs.setup({
 -- Add spaces between parentheses
 local brackets = { { '(', ')' }, { '[', ']' }, { '{', '}' } }
 npairs.add_rules {
-  npairs.Rule(' ', ' ')
+    npairs.Rule(' ', ' ')
     :with_pair(function(opts)
-      local pair = opts.line:sub(opts.col -1, opts.col)
-      return vim.tbl_contains({
-        brackets[1][1]..brackets[1][2],
-        brackets[2][1]..brackets[2][2],
-        brackets[3][1]..brackets[3][2]
-      }, pair)
+        local pair = opts.line:sub(opts.col - 1, opts.col)
+        return vim.tbl_contains({
+            brackets[1][1] .. brackets[1][2],
+            brackets[2][1] .. brackets[2][2],
+            brackets[3][1] .. brackets[3][2]
+        }, pair)
     end)
     :with_move(npairs.cond.none())
     :with_cr(npairs.cond.none())
     :with_del(function(opts)
-      local col = vim.api.nvim_win_get_cursor(0)[2]
-      local context = opts.line:sub(col - 1, col + 2)
-      return vim.tbl_contains({
-        brackets[1][1]..'  '..brackets[1][2],
-        brackets[2][1]..'  '..brackets[2][2],
-        brackets[3][1]..'  '..brackets[3][2]
-      }, context)
+        local col = vim.api.nvim_win_get_cursor(0)[2]
+        local context = opts.line:sub(col - 1, col + 2)
+        return vim.tbl_contains({
+            brackets[1][1] .. '  ' .. brackets[1][2],
+            brackets[2][1] .. '  ' .. brackets[2][2],
+            brackets[3][1] .. '  ' .. brackets[3][2]
+        }, context)
     end)
 }
-for _,bracket in pairs(brackets) do
-  npairs.Rule('', ' '..bracket[2])
-    :with_pair(npairs.cond.none())
-    :with_move(function(opts) return opts.char == bracket[2] end)
-    :with_cr(npairs.cond.none())
-    :with_del(npairs.cond.none())
-    :use_key(bracket[2])
+for _, bracket in pairs(brackets) do
+    npairs.Rule('', ' ' .. bracket[2])
+        :with_pair(npairs.cond.none())
+        :with_move(function(opts) return opts.char == bracket[2] end)
+        :with_cr(npairs.cond.none())
+        :with_del(npairs.cond.none())
+        :use_key(bracket[2])
 end
 --}}}
 
