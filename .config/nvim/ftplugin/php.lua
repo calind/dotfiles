79ab4f42640666db66635dc1274e2606184d3ca0
Calin Don <calin.don@gmail.com>
2026-06-03 13:49:32 +0300

    Skip jumping to stubs on PHP files

--- .config/nvim/ftplugin/php.lua
+++ .config/nvim/ftplugin/php.lua
@@ -1,1 +1,22 @@
+vim.b.lsp_goto_filter = function(name, opts)
+    local sources = {}
+    local stubs = {}
+
+    for _, item in ipairs(opts.items) do
+        if string.find(item.filename, 'stubs', 1, true) then
+            table.insert(stubs, item)
+        else
+            table.insert(sources, item)
+        end
+    end
+
+    if 'definition' == name and #sources >= 1 then
+        opts.items = sources
+    elseif 'declaration' == name and #stubs >= 1 then
+        opts.items = stubs
+    end
+
+    return opts
+end
+
 vim.b.lsp_formatters = { 'html', 'null-ls' }
